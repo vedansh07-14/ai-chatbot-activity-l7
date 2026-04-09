2ea7cf4f92a8343d18b7f5ac8a08ff297f7c2f29
Vedansh <vedanshsaini97@gmail.com>
2026-04-09 22:49:52 +0530

    fix: update Hugging Face image API endpoint to router.huggingface.co

--- api/image.js
+++ api/image.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch("https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0", {
+    const response = await fetch("https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0", {
       method: "POST",
       headers: {
         Authorization: `Bearer ${apiKey}`,
